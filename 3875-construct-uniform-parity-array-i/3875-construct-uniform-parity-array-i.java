b8b05d8a828a590a229102d54e88e3f755202816
Sahil Raut <sahilraut200619@gmail.com>
2026-09-03 17:57:59 +0530

    Time: 7 ms (0.2%), Space: 45.8 MB (16.17%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -1,5 +1,21 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-       return true ;
+       
+       int left=0;
+       int right=nums1.length-1;
+       int[] nums2 = new int[nums1.length];
+       while(left<right){
+       if(nums1[left]%2==0){ 
+        nums2[left]=nums1[left];
+        }
+        else if(nums1[right]%2==1){
+            nums2[right]=nums1[right];
+        }
+
+        left++;
+        right--;
+       }
+
+     return Arrays.stream(nums2).allMatch(num -> num % 2 == 0 || num % 2 == 1);
     }
 }
